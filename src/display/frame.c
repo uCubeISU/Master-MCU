f5a1fb8a623c2a6b212b6475a019427f8e99d637
burneykb18 <y3llowstrik3r18@gmail.com>
2015-03-17 19:09:58 -0500

    First commit for frame object structuring

---
+++ src/display/frame.c
@@ -0,0 +1,53 @@
+/**
+ * @addtogroup <group name>
+ * @{
+ * @file      frame.c
+ * @author    Iowa State University uCube team
+ * @author    Kristopher
+ * @date      Mar 17, 2015
+ * @brief     <brief description>
+ * @copyright Copyright (C) 2015 ISU uCube team
+ * @details   <detailed description>
+ */
+
+/*
+    Master-MCU
+    Copyright (C) 2015  Iowa State University uCube team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <frame.h>
+
+
+struct pixel
+{
+	unsigned RED:5;
+	unsigned GREEN:6;
+	unsigned BLUE:5;
+};
+
+struct panel_frame
+{
+	struct pixel data[6][6];
+};
+
+struct frame
+{
+	struct panel_frame frame[6];
+};
+
+/// @}
+
